30a2b92df5383a8810ccc728939d230123f2a668
Horimatsu Takuya <new_woory@hotmail.com>
2018-07-27 00:27:25 +0900

    initial project

---
+++ Winder4/Winder4/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Winder4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pathEdit = new System.Windows.Forms.TextBox();
+            this.filesListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // pathEdit
+            // 
+            this.pathEdit.Location = new System.Drawing.Point(12, 12);
+            this.pathEdit.Name = "pathEdit";
+            this.pathEdit.Size = new System.Drawing.Size(927, 25);
+            this.pathEdit.TabIndex = 0;
+            this.pathEdit.TextChanged += new System.EventHandler(this.PathEdit_TextChanged);
+            this.pathEdit.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PathEdit_KeyDown);
+            // 
+            // filesListBox
+            // 
+            this.filesListBox.FormattingEnabled = true;
+            this.filesListBox.ItemHeight = 18;
+            this.filesListBox.Location = new System.Drawing.Point(12, 43);
+            this.filesListBox.Name = "filesListBox";
+            this.filesListBox.Size = new System.Drawing.Size(927, 508);
+            this.filesListBox.TabIndex = 1;
+            this.filesListBox.DoubleClick += new System.EventHandler(this.filesListBox_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 552);
+            this.Controls.Add(this.filesListBox);
+            this.Controls.Add(this.pathEdit);
+            this.Name = "Form1";
+            this.Text = "Winder4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pathEdit;
+        private System.Windows.Forms.ListBox filesListBox;
+    }
+}
+
---
+++ Winder4/Winder4/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Winder4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PathEdit_TextChanged(object sender, EventArgs e)
+        {
+            var path = pathEdit.Text;
+            path = path.Replace("\\", "/");
+            pathEdit.Text = path;
+        }
+
+        private void PathEdit_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                RefreshList(pathEdit.Text);
+            }
+        }
+        private void RefreshList(string targetPath)
+        {
+            filesListBox.Items.Clear();
+
+            string[] dirs = Directory.GetDirectories(targetPath);
+            string[] files = Directory.GetFiles(targetPath, "*", SearchOption.TopDirectoryOnly);
+
+            foreach (var dir in dirs)
+            {
+                var dir2 = dir.Replace(targetPath, "").Replace("\\", "");
+                filesListBox.Items.Add(dir2);
+            }
+            foreach (var file in files)
+            {
+                var file2 = file.Replace(targetPath, "").Replace("\\", "");
+                filesListBox.Items.Add(file2);
+            }
+        }
+
+        private void filesListBox_DoubleClick(object sender, EventArgs e)
+        {
+            if (filesListBox.SelectedIndex >= 0)
+            {
+                var name = filesListBox.Items[filesListBox.SelectedIndex];
+                var targetPath = pathEdit.Text;
+                var newPath = targetPath + "/" + name;
+                if (Directory.Exists(newPath));
+                {
+                    RefreshList(newPath);
+                    pathEdit.Text = newPath;
+                }
+            }
+        }
+    }
+}
